d4cdf87521631a8206bd796516a257e8e79b9dae
Yasar Yousuf <yousuf802@gmail.com>
2026-04-30 11:52:23 +0100

    task api response test

--- app/Mail/NonCompliantEmail.php
+++ app/Mail/NonCompliantEmail.php
@@ -10,6 +10,7 @@ use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class NonCompliantEmail extends Mailable
 {
---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+pest()->use(RefreshDatabase::class);
+
+test('the application returns a successful response', function () {
+    $response = $this->get('/');
+    $response->assertOk();
+    $response->assertStatus(200);
+});
+
+test('task api response', function () {
+    User::factory()->count(3)->create();
+    Task::factory()->count(3)->create();
+    $response = $this->get('/api/task');
+    $response->assertHeader('Content-Type', 'application/json')
+        ->assertStatus(200)
+        ->assertJsonCount(3, 'data');
+});
